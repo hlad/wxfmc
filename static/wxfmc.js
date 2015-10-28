1060193e5a0cabac6d58242543c8d79cf6f7ee25
K1-K1 <k1@kokoti.org>
2015-10-28 01:12:48 +0100

    Decode display, event for hw keys

--- static/wxfmc.js
+++ static/wxfmc.js
@@ -2,7 +2,6 @@ var socket = null
 
 function fmc_click(w)
 {
-	console.log(this.id);
 	var id=this.id;
 	a=id.split('_');
 	b=a[1];
@@ -10,16 +9,10 @@ function fmc_click(w)
 	if (b&&a[0]=='fmc')
 	{
 		vibrate();
-		if (b=='14')
-		{
-			$(this).toggleClass('fmcBtnAct');
-		} else
-		{
-			$(this).addClass('fmcBtnCli');
-			setTimeout(function (){$('#'+id).removeClass('fmcBtnCli');}, 300);
-		}
+		$(this).addClass('fmcBtnCli');
+		setTimeout(function (){$('#'+id).removeClass('fmcBtnCli');}, 300);
 		//alert(b);
-		console.log(b);
+		//console.log(b);
 		socket.emit('key_event', {data: b});
 		
 	}
@@ -32,10 +25,13 @@ function fmc_init()
 {
 	socket = io.connect('http://' + document.domain + ':' + location.port);
 	$("#fmc div").bind("click",this,fmc_click);
-	socket.on('send_messages', function(data) {
-	    for (var j in data) {
-		$('#'+j.replace('/','_')).text(data[j]).html();
-	    }	    
+	socket.on('send_messages', function(data) 
+	{
+		for (var j in data)
+		{
+			line=fmc_line_decode(data[j]);
+			$('#'+j.replace('/','_')).text(line).html();
+		}
 	});
 
 	socket.on('send_status', function(status) {
@@ -51,6 +47,58 @@ function fmc_init()
 		    $('#fmc_15').addClass('fmcBtnDis'); 
 		}
 	});
+
+	$(document).keydown(function(e)
+	{
+		key=e.which;
+		console.log(key);
+		send_key=false;
+		if ((key>63)&&(key<91))
+		{
+			send_key=key-38;
+		}
+
+		if ((key>48)&&(key<58))
+		{
+			send_key=key+8;
+		}
+
+		if ((key>96)&&(key<106))
+		{
+			send_key=key-40;
+		}
+
+		switch (key)
+		{
+			case 32: send_key=53; break; // [space]
+			case 48: send_key=67; break; // 0
+			case 96: send_key=67; break; // 0
+			case 190: send_key=66; break; // .
+			case 110: send_key=66; break; // .
+			case 191: send_key=55; break; // /
+			case 111: send_key=55; break; // /
+			case 187: send_key=68; break; // +
+		}
+
+		if (send_key)
+		{
+			socket.emit('key_event', {data: send_key});
+			e.preventDefault();
+		}
+	});
+}
+
+
+function fmc_line_decode(s)
+{
+	r='                                           ';
+	a=s.replace(/[0-9]*\//,'').split(';');
+	for (i=0;i<a.length-1;++i)
+	{
+		b=a[i].match(/([0-9]*),([0-9]*),(.*)/)
+		r=stringReplace(r,b[2],b[3]);
+	}
+	return r;
 }
 
 
@@ -69,4 +117,12 @@ function menuClick(item)
 	$('#fmc').hide();
 	$('#radio').hide();
 	$('#'+item).show();
+}
+
+
+function stringReplace(str, pos, repl)
+{
+	pos=Math.ceil(pos/7);
+	console.log(str, pos, repl);
+	return str.substring(0,pos)+repl+str.substring(pos+repl.length,str.length);
 }
