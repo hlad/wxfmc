e7f46250c736d7084af3717f88b42b713888e9a1
hlad <hladma@gmail.com>
2015-10-29 10:08:18 +0100

    correct keycode for VNAV

--- static/wxfmc.js
+++ static/wxfmc.js
@@ -36,7 +36,7 @@ function fmc_init()
 
 	socket.on('send_status', function(status) {
 		if (status['LNAV']==1) { $('#fmc_71').addClass('fmcBtnAct'); } else { $('#fmc_71').removeClass('fmcBtnAct'); }
-		if (status['VNAV']==1) { $('#fmc_70').addClass('fmcBtnAct'); } else { $('#fmc_70').removeClass('fmcBtnAct'); }
+		if (status['VNAV']==1) { $('#fmc_72').addClass('fmcBtnAct'); } else { $('#fmc_72').removeClass('fmcBtnAct'); }
 		if (status['ATH']==1) { $('#fmc_69').addClass('fmcBtnAct'); } else { $('#fmc_69').removeClass('fmcBtnAct'); }
 		if (status['Execute']==1) { $('#fmc_22').addClass('fmcBtnAct'); } else { $('#fmc_22').removeClass('fmcBtnAct'); }
 		if (status['XFMC']==1) { 
